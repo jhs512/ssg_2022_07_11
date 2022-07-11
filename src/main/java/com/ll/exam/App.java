7a2694e6e03735e51216419609240b2ea98af2ed
jhs512 <jangka512@gmail.com>
2022-07-11 16:46:30 +0900

    wiseSayings에 등록시마다 생성되는 객체리모콘을 추가

--- src/main/java/com/ll/exam/App.java
+++ src/main/java/com/ll/exam/App.java
@@ -1,14 +1,19 @@
 package com.ll.exam;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
     private Scanner sc;
     private int wiseSayingsLastId;
 
+    private List<WiseSaying> wiseSayings;
+
     public App() {
         sc = new Scanner(System.in);
         wiseSayingsLastId = 0;
+        wiseSayings = new ArrayList<>();
     }
 
     public void run() {
@@ -23,12 +28,19 @@ public class App {
                 case "등록":
                     write();
                     break;
+                case "목록":
+                    list();
+                    break;
                 case "종료":
                     break outer;
             }
         }
 
         sc.close();
+    }
+
+    private void list() {
+        System.out.println(wiseSayings);
     }
 
     private void write() {
@@ -40,7 +52,7 @@ public class App {
 
         WiseSaying wiseSaying = new WiseSaying(id, content, author);
 
-        System.out.println(wiseSaying);
+        wiseSayings.add(wiseSaying);
 
         System.out.printf("%d번 명언이 등록되었습니다.\n", id);
     }
